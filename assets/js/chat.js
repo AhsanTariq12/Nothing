da91945d7c390d30da5e1a64c5d559b1b45c332e
AhsanTariq12 <107984007+AhsanTariq12@users.noreply.github.com>
2022-12-09 22:28:19 +0500

    Delete assets directory

--- assets/js/chat.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $("#addClass").click(function () {
-              $('#qnimate').addClass('popup-box-on');
-                });
-              
-                $("#removeClass").click(function () {
-              $('#qnimate').removeClass('popup-box-on');
-                });
-      })
